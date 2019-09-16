b531dd754516fac8a42cef994f644a14b6e57306
avadeshgoibibo <avadesh.kumar1@go-mmt.comgit>
2019-09-16 17:52:48 +0530

    Initial Commit

---
+++ JenkinsSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JenkinsSample
+//
+//  Created by Avadesh Kumar on 16/09/19.
+//  Copyright © 2019 MyOrganisation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
